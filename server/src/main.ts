4de1e7bde954c77b19590d4d1be7e88fffead78a
PraiseJah Ossai <118202914+praisejah-moses@users.noreply.github.com>
2026-01-13 08:58:57 +0100

    Update CORS origin for production environment

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors(
-    { origin: 'http://localhost:3000',
+    { origin: 'https://managely-production.up.railway.app',
     credentials: true
      }
   );
